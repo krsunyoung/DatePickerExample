137a4f16004aa8eba3ae65ee35317cc93e30c9df
krsunyoung <krsunyoung@naver.com>
2016-11-30 09:34:25 +0900

    re

--- app/src/main/java/com/bit2016/datepickerexample/MainActivity.java
+++ app/src/main/java/com/bit2016/datepickerexample/MainActivity.java
@@ -1,17 +1,50 @@
 package com.bit2016.datepickerexample;
 
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private DatePicker datePicker;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         datePicker = (DatePicker) findViewById(R.id.datePicker);
+
+        Calendar calendar=Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int date = calendar.get(Calendar.DATE);
+
+        datePicker.init(year, month, date, new DatePicker.OnDateChangedListener(){
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                String date = year+"-"+(monthOfYear+1)+"-"+dayOfMonth;
+                ((TextView)findViewById(R.id.dateText)).setText(date);
+            }
+        });
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int year = datePicker.getYear();
+                int month=datePicker.getMonth()+1;
+                int date=datePicker.getDayOfMonth();
+
+                String dateSelected = year + "년 "+ month +"월 "+date+"일";
+                Toast.makeText(MainActivity.this, dateSelected, Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
